4946e61f30fe9162aa3c9b6a1a4d416599e691a7
User <bruksbond@gmail.com>
2024-04-16 13:52:19 +0300

    create project rout, connect clerc authorization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+
+
 import "./globals.css";
-import { cn } from "@/lib/utils";
+import { cn } from "../lib/utils";
+
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -20,10 +24,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider appearance={{
+      variables: {colorPrimary: '#624cf5'}
+    }}>
+      <html lang="en">
+        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
